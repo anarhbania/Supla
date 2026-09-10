c48057de832a0beb998012e3edefbcc8ede0a27b
Marcin Bania <54779555+anarhbania@users.noreply.github.com>
2026-09-10 20:29:34 +0200

    Add files via upload

--- libraries/ModbusMasterRTU/src/ModbusMasterRTU.cpp
+++ libraries/ModbusMasterRTU/src/ModbusMasterRTU.cpp
@@ -22,28 +22,40 @@ ModbusMasterRTU::ModbusMasterRTU(HardwareSerial *port, uint32_t baud)
 	}
 }
 
-bool ModbusMasterRTU::isBusy()
+uint8_t ModbusMasterRTU::readHoldingRegisters(const uint8_t id, const uint16_t address, const uint16_t quantity, uint16_t *data, const uint16_t offset, uint64_t timeout)
 {
-	return busy;
-}
+	this->timeout = timeout;
 
-void ModbusMasterRTU::resetStatus()
-{
-	busy = true;
-	
-	status = MODBUS_STATUS_PREPARE;
-	
-	lastMillis = millis();
-}
+	prepare();
 
-void ModbusMasterRTU::setTimeout(uint64_t timeout)
-{
-	this->timeout = timeout;	
-}
+	tx[0] = id;
+	tx[1] = MODBUS_FUNCTION_READ_HOLDING_REGISTERS;
 
-uint64_t ModbusMasterRTU::getTimeout()
-{
-	return timeout;
+	tx[2] = (uint8_t)(address >> 8);
+	tx[3] = (uint8_t)(address & 0xFF);
+	tx[4] = (uint8_t)(quantity >> 8);
+	tx[5] = (uint8_t)(quantity & 0xFF);
+
+	uint16_t calculateCRC = calculateCRC16(tx, 6);
+
+	tx[6] = (uint8_t)(calculateCRC & 0xFF);
+	tx[7] = (uint8_t)(calculateCRC >> 8);
+
+	txQuantity = 8;
+	sendRequest();
+
+	rxQuantityResponse = 5 + 2 * quantity;
+	readResponse();
+
+	if((id == rx[0] && rx[1] == MODBUS_FUNCTION_READ_HOLDING_REGISTERS) && (calculateCRC16(rx, rxQuantity - 2) == (uint16_t)((rx[rxQuantity - 1] << 8) | rx[rxQuantity - 2])))
+	{
+		for(uint16_t i = 0; i < rx[2]; i+=2)
+		{
+			data[i] = (uint16_t)(rx[3 + i] << 8) | rx[4 + i];
+		}
+	}
+
+	return MODBUS_STATUS_OK;
 }
 
 void ModbusMasterRTU::setREDE(uint8_t pinREDE)
@@ -52,100 +64,6 @@ void ModbusMasterRTU::setREDE(uint8_t pinREDE)
 
 	pinMode(pinREDE, OUTPUT);
 	digitalWrite(pinREDE, LOW);
-}
-
-uint8_t ModbusMasterRTU::readHoldingRegisters(const uint8_t id, const uint16_t address, const uint16_t quantity, uint16_t *data, const uint16_t offset)
-{
-	function = MODBUS_FUNCTION_READ_HOLDING_REGISTERS;
-	
-	if(status == MODBUS_STATUS_PREPARE)
-	{
-		prepare();
-	}
-	else if(status == MODBUS_STATUS_REQUEST)
-	{		
-		tx[0] = id;
-		tx[1] = function;
-		
-		tx[2] = (uint8_t)(address >> 8);
-		tx[3] = (uint8_t)(address & 0xFF);
-		tx[4] = (uint8_t)(quantity >> 8);
-		tx[5] = (uint8_t)(quantity & 0xFF);
-		
-		uint16_t calculateCRC = calculateCRC16(tx, 6);
-		
-		tx[6] = (uint8_t)(calculateCRC & 0xFF);
-		tx[7] = (uint8_t)(calculateCRC >> 8);
-		
-		request(tx, 8);
-		
-		status = MODBUS_STATUS_RESPONSE;
-	}
-	else if(status == MODBUS_STATUS_RESPONSE)
-	{
-		response();
-	}
-	else if(status == MODBUS_STATUS_SAVE)
-	{
-		for(uint8_t i = 0; i < quantity; i++)
-		{
-			data[i + offset] = (rx[2 * i + 3] << 8) | rx[2 * i + 4];
-		}
-		
-		status = MODBUS_STATUS_OK;
-	}
-	else if(complite())
-	{
-		busy = false;
-	}
-	
-	return status;
-}
-
-uint8_t ModbusMasterRTU::writeMultipleRegisters(const uint8_t id, const uint16_t address, const uint16_t quantity, const uint16_t *data, const uint16_t offset)
-{
-	function = MODBUS_FUNCTION_WRITE_MULTIPLE_REGISTERS;
-	
-	if(status == MODBUS_STATUS_PREPARE)
-	{
-		prepare();
-	}
-	else if(status == MODBUS_STATUS_REQUEST)
-	{		
-		tx[0] = id;
-		tx[1] = function;
-		
-		tx[2] = (uint8_t)(address >> 8);
-		tx[3] = (uint8_t)(address & 0xFF);
-		tx[4] = (uint8_t)(quantity >> 8);
-		tx[5] = (uint8_t)(quantity & 0xFF);
-		tx[6] = (uint8_t)((2 * quantity) & 0xFF);
-		
-		for(uint8_t i = 0; i < quantity; i++)
-		{
-			tx[2 * i + 7] = (uint8_t)(data[i + offset] >> 8);
-			tx[2 * i + 8] = (uint8_t)(data[i + offset] & 0xFF);
-		}
-		
-		uint16_t calculateCRC = calculateCRC16(tx, 2 * quantity + 7);
-		
-		tx[2 * quantity + 7] = (uint8_t)(calculateCRC & 0xFF);
-		tx[2 * quantity + 8] = (uint8_t)(calculateCRC >> 8);
-		
-		request(tx, 2 * quantity + 9);
-		
-		status = MODBUS_STATUS_RESPONSE;
-	}
-	else if(status == MODBUS_STATUS_RESPONSE)
-	{
-		response();
-	}
-	else if(complite())
-	{
-		busy = false;
-	}
-	
-	return status;
 }
 
 uint16_t ModbusMasterRTU::conversionToUint16(uint32_t variable, bool bigEndian)
@@ -181,30 +99,29 @@ void ModbusMasterRTU::prepare()
 {
 	if(millis() - lastMillis > 50)
 	{
-		status = MODBUS_STATUS_REQUEST;
-		
+		txQuantity = 0;
+		rxQuantity = 0;
+
 		memset(tx, 0, sizeof(tx));
 		memset(rx, 0, sizeof(rx));
-		
-		lastMillis = millis();
 	}
 }
 
-void ModbusMasterRTU::request(uint8_t *data, uint8_t length)
+void ModbusMasterRTU::sendRequest()
 {
 	if(pinREDE != -1)
 	{
 		digitalWrite(pinREDE, HIGH);
 	}
-	
-	for(uint8_t i = 0; i < length; i++)
+
+	for(uint8_t i = 0; i < txQuantity; i++)
 	{
-		(*port).write(data[i]);
+		(*port).write(tx[i]);
 	}
 
-	(*port).flush();
-
 	delayMicroseconds(t3_5);
+
+	(*port).flush();
 
 	if(pinREDE != -1)
 	{
@@ -212,69 +129,29 @@ void ModbusMasterRTU::request(uint8_t *data, uint8_t length)
 	}
 }
 
-void ModbusMasterRTU::response()
+void ModbusMasterRTU::readResponse()
 {
-	if((*port).available())
+	lastMillis = millis();
+	
+	while((millis() - lastMillis < timeout) && rxQuantity < rxQuantityResponse)
 	{
-		lastMillis = millis();
-		
-		uint8_t frameQuantity = 0;
-		
-		while((*port).available())
+		if((*port).available())
 		{
-			if(frameQuantity == FRAME_SIZE)
+			while((*port).available())
 			{
-				rx[FRAME_SIZE] = (*port).read();
-			}
-			else
-			{
-				rx[frameQuantity++] = (*port).read();
-			}
-			
-			delayMicroseconds(t1_5);
-		}
-		
-		uint16_t calculateCRC = calculateCRC16(rx, frameQuantity - 2);
-		
-		if(calculateCRC == (((rx[frameQuantity - 1] << 8) | rx[frameQuantity - 2])))
-		{
-			if(function == MODBUS_FUNCTION_READ_HOLDING_REGISTERS)
-			{
-				if(rx[1] == MODBUS_FUNCTION_READ_HOLDING_REGISTERS)
+				if(rxQuantity == FRAME_SIZE)
 				{
-					status = MODBUS_STATUS_SAVE;
+					rx[FRAME_SIZE] = (*port).read();
 				}
 				else
 				{
-					status = MODBUS_STATUS_ILLEGAL_DATA_FUNCTION;
+					rx[rxQuantity++] = (*port).read();
 				}
-			}
-			else if(function == MODBUS_FUNCTION_WRITE_MULTIPLE_REGISTERS)
-			{
-				if(rx[1] == MODBUS_FUNCTION_WRITE_MULTIPLE_REGISTERS)
-				{
-					status = MODBUS_STATUS_OK;
-				}
-				else
-				{
-					status = MODBUS_STATUS_ILLEGAL_DATA_FUNCTION;
-				}
+				
+				delayMicroseconds(t1_5);
 			}
 		}
-		else
-		{
-			status = MODBUS_STATUS_ERROR_CRC;
-		}
 	}
-	else if(millis() - lastMillis > timeout)
-	{
-		status = MODBUS_STATUS_ERROR_TIMEOUT;
-	}
-}
-
-bool ModbusMasterRTU::complite()
-{
-	return status == MODBUS_STATUS_OK || status == MODBUS_STATUS_ERROR_TIMEOUT || status == MODBUS_STATUS_ERROR_CRC || status == MODBUS_STATUS_ILLEGAL_DATA_FUNCTION || status == MODBUS_STATUS_ILLEGAL_DATA_ADDRESS || status == MODBUS_STATUS_ILLEGAL_DATA_VALUE;
 }
 
 uint16_t ModbusMasterRTU::calculateCRC16(uint8_t *data, uint8_t length)
--- libraries/ModbusMasterRTU/src/ModbusMasterRTU.h
+++ libraries/ModbusMasterRTU/src/ModbusMasterRTU.h
@@ -10,34 +10,34 @@
 
 #define FRAME_SIZE  512
 
-enum ModbusError : uint8_t
+enum ModbusMasterError : uint8_t
 {
-	MODBUS_ERROR_NONE,
-	MODBUS_ERROR_ILLEGAL_DATA_FUNCTION = 0x01,
-	MODBUS_ERROR_ILLEGAL_DATA_ADDRESS = 0x02,
-	MODBUS_ERROR_ILLEGAL_DATA_VALUE = 0x03
+	MODBUS_MASTER_ERROR_NONE,
+	MODBUS_MASTER_ERROR_ILLEGAL_DATA_FUNCTION = 0x01,
+	MODBUS_MASTER_ERROR_ILLEGAL_DATA_ADDRESS = 0x02,
+	MODBUS_MASTER_ERROR_ILLEGAL_DATA_VALUE = 0x03
 };
 
-enum ModbusFunction : uint8_t
+enum ModbusMasterFunction : uint8_t
 {
-	MODBUS_FUNCTION_NONE,
-	MODBUS_FUNCTION_READ_HOLDING_REGISTERS = 0x03,
-	MODBUS_FUNCTION_WRITE_SINGLE_REGISTER = 0x06,
-	MODBUS_FUNCTION_WRITE_MULTIPLE_REGISTERS = 0x10
+	MODBUS_MASTER_FUNCTION_NONE,
+	MODBUS_MASTER_FUNCTION_READ_HOLDING_REGISTERS = 0x03,
+	MODBUS_MASTER_FUNCTION_WRITE_SINGLE_REGISTER = 0x06,
+	MODBUS_MASTER_FUNCTION_WRITE_MULTIPLE_REGISTERS = 0x10
 };
 
-enum ModbusStatus : uint8_t
+enum ModbusMasterStatus : uint8_t
 {
-	MODBUS_STATUS_PREPARE,
-	MODBUS_STATUS_REQUEST,
-	MODBUS_STATUS_RESPONSE,
-	MODBUS_STATUS_SAVE,
-	MODBUS_STATUS_OK,
-	MODBUS_STATUS_ERROR_TIMEOUT,
-	MODBUS_STATUS_ERROR_CRC,
-	MODBUS_STATUS_ILLEGAL_DATA_FUNCTION,
-	MODBUS_STATUS_ILLEGAL_DATA_ADDRESS,
-	MODBUS_STATUS_ILLEGAL_DATA_VALUE
+	MODBUS_MASTER_STATUS_PREPARE,
+	MODBUS_MASTER_STATUS_REQUEST,
+	MODBUS_MASTER_STATUS_RESPONSE,
+	MODBUS_MASTER_STATUS_SAVE,
+	MODBUS_MASTER_STATUS_OK,
+	MODBUS_MASTER_STATUS_ERROR_TIMEOUT,
+	MODBUS_MASTER_STATUS_ERROR_CRC,
+	MODBUS_MASTER_STATUS_ILLEGAL_DATA_FUNCTION,
+	MODBUS_MASTER_STATUS_ILLEGAL_DATA_ADDRESS,
+	MODBUS_MASTER_STATUS_ILLEGAL_DATA_VALUE
 };
 
 class ModbusMasterRTU
@@ -45,18 +45,11 @@ class ModbusMasterRTU
 	public:
 
 	ModbusMasterRTU(HardwareSerial *port, uint32_t baud);
-	
-	bool isBusy();
-	void resetStatus();
-	
-	void setTimeout(uint64_t timeout);
-	uint64_t getTimeout();
-	
+
+	uint8_t readHoldingRegisters(const uint8_t id, const uint16_t address, const uint16_t quantity, uint16_t *data, const uint16_t offset, uint64_t timeout);
+
 	void setREDE(uint8_t pinREDE);
-	
-	uint8_t readHoldingRegisters(const uint8_t id, const uint16_t address, const uint16_t quantity, uint16_t *data, const uint16_t offset);
-	uint8_t writeMultipleRegisters(const uint8_t id, const uint16_t address, const uint16_t quantity, const uint16_t *data, const uint16_t offset);
-	
+
 	uint16_t conversionToUint16(uint32_t variable, bool bigEndian);
 	uint32_t conversionToUint32(uint16_t variable0, uint16_t variable1, bool bigEndian);
 	float conversionToFloat(uint32_t variable);
@@ -64,29 +57,28 @@ class ModbusMasterRTU
 	protected:
 
 	void prepare();
-	void request(uint8_t *data, uint8_t length);
-	void response();
-	bool complite();
-	
+
+	void sendRequest();
+	void readResponse();
+
 	uint16_t calculateCRC16(uint8_t *data, uint8_t length);
 
 	HardwareSerial *port;
 
-	bool busy = true;
-
 	uint8_t pinREDE = -1;
-	uint8_t error = MODBUS_ERROR_NONE;
-	uint8_t function = MODBUS_FUNCTION_NONE;
-	uint8_t status = MODBUS_STATUS_PREPARE;
 
 	uint8_t tx[FRAME_SIZE] = {};
 	uint8_t rx[FRAME_SIZE] = {};
 
+	uint16_t txQuantity = 0;
+	uint16_t rxQuantity = 0;
+	uint16_t rxQuantityResponse = 0;
+
 	uint16_t t1_5;
 	uint16_t t3_5;
 
-	uint64_t timeout = 500;
-	uint64_t lastMillis;
+	uint64_t timeout = 0;
+	uint64_t lastMillis = 0;
 };
 
 #endif
